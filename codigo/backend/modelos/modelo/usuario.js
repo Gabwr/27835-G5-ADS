d5e07e2a74cff3d5f1b6b3e71d6d2098dd157aa9
unknown <gmlopez11@espe.edu.ec>
2025-12-10 13:56:53 -0500

    creacion modelo usuario backend

--- codigo/backend/modelos/modelo/usuario.js
+++ codigo/backend/modelos/modelo/usuario.js
@@ -1,6 +1,45 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../base_de_datos');
+const bcrypt = require('bcrypt');
 
-const usuario = sequelize.define('USUARIO', {
-
+const usuario = sequelize.define('ususario',{
+  usuario_id: {
+    type: DataTypes.STRING(32),
+    primaryKey: true,
+    allowNull: false,
+  },
+  usuario_usuario: {
+    type: DataTypes.STRING(32),
+    allowNull: false,
+    unique: true,
+    field: 'usuario_usuario' 
+  },
+  usuario_contrasenia: {
+    type: DataTypes.STRING(64),
+    allowNull: false,
+    field: 'usuario_contrasenia'
+  }
+}, {
+  tableName: 'usuario',     
+  timestamps: false,       
+  hooks: {
+    beforeCreate: async (usuario) => {
+      if (usuario.usuario_contrasenia) {
+        const saltRounds = 10;
+        usuario.usuario_contrasenia = await bcrypt.hash(usuario.usuario_contrasenia, saltRounds);
+      }
+    },
+    beforeUpdate: async (usuario) => {
+      if (usuario.changed('usuario_contrasenia')) {
+        const saltRounds = 10;
+        usuario.usuario_contrasenia= await bcrypt.hash(usuario.usuario_contrasenia, saltRounds);
+      }
+    }
+  }
 });
+
+Usuario.prototype.validarContrasenia = async function(contrasenia) {
+  return await bcrypt.compare(contrasenia, this.usuario_contrasenia);
+};
+
+module.exports = Usuario;
