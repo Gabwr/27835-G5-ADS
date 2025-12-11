0e9d8554df31011df065170ba324b18c1eac30c8
Juan Diego Delgado Arciniega <jddelgado5@espe.edu.ec>
2025-12-11 09:32:53 -0500

    Inclusion del panel

--- codigo/frontend/src/App.jsx
+++ codigo/frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 import Menu from './componentes/menu/menu'
+import { Routes, Route } from 'react-router-dom'
+import ManejoCuidados from './paginas/cuidados/ManejoCuidados'
+import GestionarMontas from './paginas/cuidados/GestionarMontas'
+import GestionarAlimentacion from './paginas/cuidados/GestionarAlimentacion'
+import GestionarVacunacion from './paginas/cuidados/GestionarVacunacion'
 function App() {
 
   return (
     <>
       <Menu />
+      <Routes>
+        <Route path="/cuidados" element={<ManejoCuidados />} />
+        <Route path="/cuidados/montas" element={<GestionarMontas />} />
+        <Route path="/cuidados/alimentacion" element={<GestionarAlimentacion />} />
+        <Route path="/cuidados/vacunacion" element={<GestionarVacunacion />} />
+      </Routes>
     </>
   )
 }
 
-export {App} 
+export { App } 
